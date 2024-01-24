8dd5102896de85f663a4bcd8c8edb8d7951f68ff
SahilManiya <sahilmaniya67@gmail.com>
2024-01-24 17:47:01 +0530

    second commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,38 +25,18 @@ module.exports.categoryData = async(req,res)=>{
         let CatData = await Category.find({});
         let SubcatData = await Subcategory.find({});
         let ExcatData = await Extracategory.find({});
-        let productData = await Product.find({category:req.params.cateGoryId,subcategory:req.params.subcateGoryId,extracategory:req.params.extracateGoryId});
+        let productData = await Product.find({category:req.params.cid,subcategory:req.params.sid,extracategory:req.params.eid});
         return res.render('user_panel/CategoryData',{
             CatData : CatData,
             SubcatData : SubcatData,
             ExcatData : ExcatData,
             ProData : productData,
-            cateGoryId : req.params.cateGoryId,
-            subcategory : req.params.subcateGoryId,
-            extracategory : req.params.extracateGoryId
         })
     } 
     catch (error) {
         console.log(error);
         return res.redirect('back');    
     }
-
-    // try {
-    //     let CatData = await Category.find({});
-    //     let SubcatData = await Subcategory.find({});
-    //     let ExcatData = await Extracategory.find({});
-    //     let ProData = await Product.find({});
-    //     return res.render('user_panel/CategoryData',{
-    //         CatData : CatData,
-    //         SubcatData : SubcatData,
-    //         ExcatData : ExcatData,
-    //         ProData : ProData
-    //     });
-    // } 
-    // catch (error) {
-    //     console.log(error);
-    //     return res.redirect('back');    
-    // }
 }
 
 module.exports.Product_Page = async(req,res)=>{
--- router/user.js
+++ router/user.js
@@ -4,7 +4,7 @@ const UserController = require('../controllers/UserController');
 
 routes.get('/',UserController.user_home);
 
-routes.get('/categoryData/:cateGoryId/:subcateGoryId/:extracateGoryId',UserController.categoryData);
+routes.get('/categoryData/:cid/:sid/:eid',UserController.categoryData);
 
 routes.get('/Product_Page/:id',UserController.Product_Page);
 
